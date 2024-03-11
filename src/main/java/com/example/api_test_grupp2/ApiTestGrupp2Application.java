7a634680850b3f9c74348b33fe670481f964e882
Turgay S <tsokmen76@gmail.com>
2024-03-11 12:41:42 +0100

    Initial commit

---
+++ src/main/java/com/example/api_test_grupp2/ApiTestGrupp2Application.java
@@ -0,0 +1,13 @@
+package com.example.api_test_grupp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestGrupp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestGrupp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_test_grupp2/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.example.api_test_grupp2;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "pretty"
+        },
+        features = "src/test/java/com/resources/features"
+
+
+)
+
+
+
+
+public class CukesRunner {
+}
